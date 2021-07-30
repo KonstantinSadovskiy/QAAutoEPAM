1e49dda9553a752205e38e030f460dbb1054a68a
Konstantin Sadovskiy <kostiasadovskiy@gmail.com>
2021-07-30 16:18:24 +0300

    Minor changes

--- OOP/OOP/Helper.cs
+++ OOP/OOP/Helper.cs
@@ -9,21 +9,21 @@ namespace OOP
 {
     public static class Helper
     {
-        public static List<Transport> ApplyAboveVolumeValueFilter(AutoPark autoPark, double volumeValue)
+        public static List<Transport> GetTransportAboveVolumeValueFilter(AutoPark autoPark, double volumeValue)
         {
             return autoPark.TransportPark.Where(transportItem => transportItem.Engine.Volume > volumeValue)
                                          .OrderBy(transportItem => transportItem.Engine.Power)
                                          .ToList();
         }
 
-        public static List<Transport> ApplyBusOrTruckFilter(AutoPark autoPark)
+        public static List<Transport> GetTransportBusOrTruckFilter(AutoPark autoPark)
         {
             return autoPark.TransportPark.Where(transportItem => (transportItem is Bus || 
                                                                   transportItem is Truck))
                                          .ToList();
         }
 
-        public static List<Transport> ApplyGroupByTransmissionFilter(AutoPark autoPark)
+        public static List<Transport> GetTransportGroupByTransmissionFilter(AutoPark autoPark)
         {
             return autoPark.TransportPark.GroupBy(transportItem => transportItem.Transmission.Type).SelectMany(transportItem => transportItem).ToList();
         }
