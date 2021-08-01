af7c16e47ac454cb6bb8459be9a900fa583dbaa0
Konstantin Sadovskiy <kostiasadovskiy@gmail.com>
2021-08-01 21:54:04 +0300

    minor fix

--- DevelopmentAndBuildTool/DevelopmentAndBuildTool/StringOperations.cs
+++ DevelopmentAndBuildTool/DevelopmentAndBuildTool/StringOperations.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DevelopmentAndBuildTools
+﻿namespace DevelopmentAndBuildTools
 {
     public static class StringOperations
     {
